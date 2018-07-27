6e4e53d0cf0bb777910802a5ed25af9cd4bd66cc
Poline Fedorova <polina.fedorova@devexpress.com>
2018-07-27 12:12:11 +0300

    add files

---
+++ CS/MailMergeEmail/Form1.cs
@@ -0,0 +1,33 @@
+﻿using DevExpress.XtraEditors;
+using DevExpress.XtraRichEdit;
+using Outlook = Microsoft.Office.Interop.Outlook;
+using System;
+
+namespace MailMergeEmail
+{
+    public partial class Form1 : XtraForm
+    {
+        RichEditDocumentServer server;
+        public Form1()
+        {
+            InitializeComponent();
+            server = new RichEditDocumentServer();
+            server.LoadDocument("MailMergeSimple.docx");
+        }
+        private void SendAnEmail()
+        {
+            Outlook.Application application = new Outlook.Application();
+            Outlook.MailItem mailItem = (Outlook.MailItem)application.CreateItem(Outlook.OlItemType.olMailItem);
+
+            RichEditMailMessageExporter exporter = new RichEditMailMessageExporter(server, mailItem);
+            exporter.Export();
+
+            mailItem.Display(false);
+        }
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+            SendAnEmail();
+        }
+    }
+}
---
+++ CS/MailMergeEmail/RichEditMailMessageExporter.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Text;
+using DevExpress.XtraRichEdit;
+using DevExpress.Office.Services;
+using Outlook = Microsoft.Office.Interop.Outlook;
+using System.IO;
+using DevExpress.Utils;
+using DevExpress.Office.Utils;
+using System.Drawing.Imaging;
+using DevExpress.XtraRichEdit.Export;
+
+namespace MailMergeEmail
+{
+    public class RichEditMailMessageExporter : IUriProvider
+    {
+        readonly RichEditDocumentServer server;
+        readonly Outlook.MailItem mailItem;
+
+        public RichEditMailMessageExporter(RichEditDocumentServer richServer, Outlook.MailItem mailItem)
+        {
+            Guard.ArgumentNotNull(richServer, "control");
+            Guard.ArgumentNotNull(mailItem, "mailItem");
+
+            this.server = richServer;
+            this.mailItem = mailItem;
+        }
+        string tempFiles = Path.Combine(Directory.GetCurrentDirectory(), "TempFiles");
+
+        public virtual void Export()
+        {
+            if (!Directory.Exists(tempFiles))
+                Directory.CreateDirectory(tempFiles);
+
+            server.BeforeExport += OnBeforeExport;
+            string htmlBody = server.Document.GetHtmlText(server.Document.Range, this);
+            server.BeforeExport -= OnBeforeExport;
+
+            mailItem.BodyFormat = Outlook.OlBodyFormat.olFormatHTML;
+            mailItem.HTMLBody = htmlBody;
+        }
+        private void OnBeforeExport(object sender, BeforeExportEventArgs e)
+        {
+            HtmlDocumentExporterOptions options = e.Options as HtmlDocumentExporterOptions;
+            if (options != null)
+            {
+                options.Encoding = Encoding.UTF8;
+            }
+        }
+        int imageId;
+
+        public string CreateCssUri(string rootUri, string styleText, string relativeUri)
+        {
+            return String.Empty;
+        }
+
+        public string CreateImageUri(string rootUri, OfficeImage image, string relativeUri)
+        {
+            string imageName = String.Format("image{0}.png", imageId);
+            imageId++;
+
+            string imagePath = Path.Combine(tempFiles, imageName);
+
+            image.NativeImage.Save(imagePath, ImageFormat.Png);
+
+            mailItem.Attachments.Add(imagePath, Outlook.OlAttachmentType.olByValue, 0, Type.Missing);
+
+            return "cid:" + imageName;
+        }
+    }
+}
